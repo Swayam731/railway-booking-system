acc46efb7eb829007beb7278da932db7683dbb9e
swayam <swayamsahu26421@gmail.com>
2024-10-29 13:36:05 +0530

    deployment files

---
+++ bin/render-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+
+#if you have seeds to run add:
+bundle exec rails db:seed
